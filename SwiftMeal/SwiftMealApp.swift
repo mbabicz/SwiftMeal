916b15226adb7f7174f0f2e1a8389de811899bbf
Michał Babicz <kiwuzx@gmail.com>
2023-04-03 22:25:39 +0200

    Initial Commit

---
+++ SwiftMeal/SwiftMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMealApp.swift
+//  SwiftMeal
+//
+//  Created by kz on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
