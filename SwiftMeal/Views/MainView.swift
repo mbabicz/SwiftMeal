95f3a1287de91c46b1d90fed69ffda9e1b5abb46
Michał Babicz <kiwuzx@gmail.com>
2023-04-06 20:52:40 +0200

    Category picker

--- SwiftMeal/Views/MainView.swift
+++ SwiftMeal/Views/MainView.swift
@@ -7,21 +7,64 @@
 
 import SwiftUI
 
+enum Category: String, CaseIterable {
+    case Beef = "Beef"
+    case Chicken = "Chicken"
+    case Dessert = "Dessert"
+    case Lamb = "Lamb"
+    case Miscellaneous = "Miscellaneous"
+    case Pasta = "Pasta"
+    case Pork = "Pork"
+    case Seafood = "Seafood"
+    case Side = "Side"
+    case Starter = "Starter"
+    case Vegan = "Vegan"
+    case Vegetarian = "Vegetarian"
+    case Breakfast = "Breakfast"
+    case Goat = "Goat"
+}
+
 struct MainView: View {
     
     @ObservedObject var mealViewModel = MealViewModel()
     
     @State var searchText = ""
+    @State var selectedCategory: Category = .Beef
 
     var body: some View {
         NavigationView{
-            VStack{
+            VStack(alignment: .leading, spacing: 0){
                 if mealViewModel.meal != nil {
                     NavigationLink(destination: MealDetailsView(meal: (mealViewModel.meal!))){
                         MealCard(meal: (mealViewModel.meal!))
                     }
                 }
-                Spacer()
+                Text("Categories")
+                    .padding(.horizontal, 20)
+                    .font(.title)
+                    .bold()
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 10) {
+                        ForEach(Category.allCases, id: \.self) { category in
+                            Button(action: {
+                                self.selectedCategory = category
+                            }) {
+                                Text(category.rawValue)
+                                    .font(.callout)
+                                    .foregroundColor(category == selectedCategory ? .white : .black)
+                                    .padding(.vertical, 10)
+                                    .padding(.horizontal, 20)
+                                    .background(category == selectedCategory ? Color.blue : Color.gray.opacity(0.4))
+                                    .cornerRadius(25)
+                            }
+                        }
+                    }
+                    .padding(.horizontal)
+                    
+                    Spacer()
+                }
+                .padding(.vertical, 10)
             }
             .onChange(of: searchText) { searchText in
                 mealViewModel.searchMealByName(name: searchText)
