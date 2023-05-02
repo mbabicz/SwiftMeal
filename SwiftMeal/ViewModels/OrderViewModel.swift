036cbdeb875078fd865209f0b4439eb46cc14e2f
Michał Babicz <kiwuzx@gmail.com>
2023-05-02 19:08:37 +0200

    sorting orders by date/status

--- SwiftMeal/ViewModels/OrderViewModel.swift
+++ SwiftMeal/ViewModels/OrderViewModel.swift
@@ -72,6 +72,5 @@ class OrderViewModel: ObservableObject {
             }
         }
     }
-
     
 }
--- SwiftMeal/Views/OrderHistoryView.swift
+++ SwiftMeal/Views/OrderHistoryView.swift
@@ -10,12 +10,35 @@ import SwiftUI
 struct OrderHistoryView: View {
     
     @EnvironmentObject var orderViewModel: OrderViewModel
-
+    
     var body: some View {
-        ForEach(orderViewModel.orders) { order in
-            Text(order.formattedDate)
-        }
         
+        let activeOrders = orderViewModel.orders.filter { $0.isActive }
+        let inactiveOrders = orderViewModel.orders.filter { !$0.isActive }
+        let sortedOrders = activeOrders + inactiveOrders.sorted(by: { $0.date > $1.date })
+        
+        ForEach(sortedOrders) { order in
+            VStack{
+                HStack{
+                    Text(order.formattedDate)
+                    Spacer()
+                    Text(order.status)
+                }
+                .padding(.horizontal)
+                
+                HStack(alignment: .bottom){
+                    VStack{
+                        
+                        ForEach(order.products.sorted(by: <), id: \.key) { (product, amount) in
+                            Text("\(product) \(amount)")
+                        }
+                    }
+                    Text(order.formattedTotalPrice)
+                }
+            }
+            .padding()
+        }
+
     }
 }
 
