c72ea8285607a6e07f72d503e5a498423a7c3315
Michał Babicz <kiwuzx@gmail.com>
2023-05-05 20:24:30 +0200

    order history sections

--- SwiftMeal/Views/OrderHistoryView.swift
+++ SwiftMeal/Views/OrderHistoryView.swift
@@ -10,54 +10,90 @@ import SwiftUI
 struct OrderHistoryView: View {
     
     @EnvironmentObject var orderViewModel: OrderViewModel
-    @EnvironmentObject var mealViewModel: MealViewModel
-
+    
     
     var body: some View {
         
         let activeOrders = orderViewModel.orders.filter { $0.isActive }
         let inactiveOrders = orderViewModel.orders.filter { !$0.isActive }
-        let sortedOrders = activeOrders + inactiveOrders.sorted(by: { $0.date > $1.date })
         
         ScrollView{
-            ForEach(sortedOrders) { order in
-                ZStack{
-                    RoundedRectangle(cornerRadius: 10, style: .continuous)
-                        .fill(.white)
-                        .shadow(radius: 10)
-                    VStack{
-                        HStack{
-                            Text(order.formattedDate).foregroundColor(.gray)
-                            Spacer()
-                            Text(order.status).foregroundColor(order.isActive ? .green : .black)
-                        }
-                        .padding(.vertical)
-                        
-                        VStack(alignment: .leading){
-                            
-                            ForEach(order.products.sorted(by: <), id: \.key) { (productID, amount) in
-                                if let meal = mealViewModel.meals?.first(where: { $0.id == productID }) {
-                                    Text("\(amount)x \(meal.name)")
-                                }
-                            }
-                            
-                            HStack{
-                                Spacer()
-                                Text(order.formattedTotalPrice)
-                                    .bold()
-                            }
-                        }
+            if !activeOrders.isEmpty {
+                Section(header:
+                     Text("Active")
+                         .font(.largeTitle)
+                         .foregroundColor(.black)
+                         .frame(maxWidth: .infinity, alignment: .leading)
+                         .padding(.horizontal)
+                         .bold()
+                ){
+                    ForEach(activeOrders) { order in
+                        OrderHistoryCardView(order: order)
                     }
-                    .padding(.horizontal)
-                    .padding(.bottom, 15)
                 }
-                .padding(5)
-                .padding(.horizontal, 5)
+            }
+            
+            if !inactiveOrders.isEmpty {
+                Section(header:
+                     Text("History")
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    .bold()
+                ){
+                    ForEach(inactiveOrders) { order in
+                        OrderHistoryCardView(order: order)
+                    }
+                }
             }
         }
     }
 }
+        
+struct OrderHistoryCardView: View {
+    
+    @EnvironmentObject var mealViewModel: MealViewModel
+    var order: Order
+    
+    var body: some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                .fill(.white)
+                .shadow(radius: 10)
+            VStack{
+                HStack{
+                    Text(order.formattedDate).foregroundColor(.gray)
+                    Spacer()
+                    Text(order.status).foregroundColor(order.isActive ? .green : .black)
+                }
+                .padding(.vertical)
+                
+                VStack(alignment: .leading){
+                    
+                    ForEach(order.products.sorted(by: <), id: \.key) { (productID, amount) in
+                        if let meal = mealViewModel.meals?.first(where: { $0.id == productID }) {
+                            Text("\(amount)x \(meal.name)")
+                        }
+                    }
+                    
+                    HStack{
+                        Spacer()
+                        Text(order.formattedTotalPrice)
+                            .bold()
+                    }
+                }
+            }
+            .padding(.horizontal)
+            .padding(.bottom, 15)
+        }
+        .padding(5)
+        .padding(.horizontal, 5)
+    }
+    
+}
 
+            
 struct OrderHistoryView_Previews: PreviewProvider {
     static var previews: some View {
         OrderHistoryView()
