ed143b7be798c246cae1f7878b3e416963863855
Michał Babicz <kiwuzx@gmail.com>
2023-07-14 15:20:56 +0200

    Create SwiftMealDelivery target for delivery app

---
+++ SwiftMealDelivery/SwiftMealDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMealDeliveryApp.swift
+//  SwiftMealDelivery
+//
+//  Created by mbabicz on 14/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMealDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
