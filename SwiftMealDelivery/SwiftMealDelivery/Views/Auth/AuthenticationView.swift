f7e8a721dadd6e39422318193251b3d19937dd8b
Michał Babicz <kiwuzx@gmail.com>
2023-08-03 17:43:37 +0200

    AuthenticationWrapper design

--- SwiftMealDelivery/SwiftMealDelivery/Views/Auth/AuthenticationView.swift
+++ SwiftMealDelivery/SwiftMealDelivery/Views/Auth/AuthenticationView.swift
@@ -9,31 +9,60 @@ import SwiftUI
 
 struct AuthenticationView: View {
     @EnvironmentObject var vm: AuthViewModel
-    
+
     var body: some View {
         VStack {
             if vm.activeSheet == nil {
                 VStack {
-                    Button {
-                        vm.activeSheet = .signIn
-                    } label: {
-                        Text("Sign In")
-                            .padding()
-                            .foregroundColor(.black)
-                            .background(RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 1))
+                    Spacer()
+                    HStack{
+                        Image("burger")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 65, height: 65)
+                        VStack(){
+                            Text("SwiftMeal")
+                                .font(.custom("HelveticaNeue-Bold", size: 45))
+                                .fontWeight(.bold)
+                                .foregroundColor(.orange)
+                            Text("Delivery")
+                                .frame(alignment: .trailing)
+                                .font(.custom("HelveticaNeue-Bold", size: 20))
+                        }
                     }
-                    .padding()
+                    .padding(.vertical)
 
-                    Button {
-                        vm.activeSheet = .signUp
-                    } label: {
-                        Text("Sign Up")
+                    Spacer()
+                    
+                    Button(action: {
+                        vm.activeSheet = .signIn
+                    }) {
+                        Text("Sign In")
+                            .foregroundColor(.white)
                             .padding()
-                            .foregroundColor(.black)
-                            .background(RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 1))
+                            .frame(maxWidth: .infinity)
+                            .background(Color.black)
+                            .cornerRadius(10)
                     }
-                    .padding()
+                    
+                    Button(action: {
+                        vm.activeSheet = .signUp
+                    }) {
+                        Text("Sign Up")
+                            .foregroundColor(.black)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.black, lineWidth: 1)
+                            )
+                    }
+                    Spacer()
                 }
+                .padding()
+                
+                Spacer()
+                
             } else {
                 if vm.activeSheet == .signIn {
                     SignInView()
@@ -46,4 +75,3 @@ struct AuthenticationView: View {
         }
     }
 }
-
