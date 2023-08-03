ff5ddd4f117a1b34654c8c7710289d61fabb6fe0
Michał Babicz <kiwuzx@gmail.com>
2023-08-03 16:15:45 +0200

    SignUp design, forms, transitions

--- SwiftMealDelivery/SwiftMealDelivery/Views/Auth/SignUpView.swift
+++ SwiftMealDelivery/SwiftMealDelivery/Views/Auth/SignUpView.swift
@@ -11,12 +11,145 @@ struct SignUpView: View {
     
     @EnvironmentObject var vm: AuthViewModel
 
+    @State private var showEmailForm = false
+    @State private var showOptions = true
+    @State private var email = ""
+    @State private var password = ""
+    @State private var passwordConfirmation = ""
+
     var body: some View {
-        Button {
-            vm.activeSheet = .signIn
-        } label: {
-            Text("signIn")
+        VStack(spacing: 20) {
+            
+            Spacer()
+            
+            HStack{
+                Image("burger")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 65, height: 65)
+                VStack(){
+                    Text("SwiftMeal")
+                        .font(.custom("HelveticaNeue-Bold", size: 45))
+                        .fontWeight(.bold)
+                        .foregroundColor(.orange)
+                    Text("Delivery")
+                        .frame(alignment: .trailing)
+                        .font(.custom("HelveticaNeue-Bold", size: 20))
+                }
+            }
+            
+            Spacer(minLength: 20)
+            
+            if showOptions {
+                Button(action: {
+                    // google auth
+                }) {
+                    HStack {
+                        Image("google_logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20)
+                        Text("Sign up with Google")
+                        
+                        Spacer()
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(12)
+                    .foregroundColor(.black)
+                    .overlay(RoundedRectangle(cornerRadius: 12).stroke(.black, lineWidth: 0.5))
+
+                }
+                
+                Button(action: {
+                    withAnimation {
+                        self.showEmailForm.toggle()
+                        self.showOptions.toggle()
+                    }
+                }) {
+                    HStack {
+                        Image(systemName: "envelope.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20)
+                        Text("Sign up with Email")
+                        
+                        Spacer()
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.orange)
+                    .cornerRadius(12)
+                    .foregroundColor(.white)
+                    .overlay(RoundedRectangle(cornerRadius: 12).stroke(.black, lineWidth: 0.5))
+
+                }
+                
+                Spacer()
+                
+                Button {
+                    vm.activeSheet = .signIn
+                } label: {
+                    HStack {
+                        Text("Have an account?")
+                        Text("Log In")
+                            .underline()
+                    }
+                    .foregroundColor(.black)
+                }
+
+            }
+            
+            if showEmailForm {
+                VStack(spacing: 20) {
+                    TextField("Email", text: $email)
+                        .padding()
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(12)
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(12)
+                    SecureField("Confirm password", text: $passwordConfirmation)
+                        .padding()
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(12)
+                        .padding(.bottom)
+                    
+                    Button(action: {
+                        // e-mail auth
+                    }) {
+                        Text("Sign Up")
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.orange)
+                            .cornerRadius(12)
+                            .foregroundColor(.white)
+                            .overlay(RoundedRectangle(cornerRadius: 12).stroke(.black, lineWidth: 0.5))
+                    }
+                    
+                    Button(action: {
+                        withAnimation {
+                            self.showEmailForm.toggle()
+                            self.showOptions.toggle()
+                        }
+                    }) {
+                        Text("Back")
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.gray.opacity(0.2))
+                            .cornerRadius(12)
+                            .overlay(RoundedRectangle(cornerRadius: 12).stroke(.black, lineWidth: 0.5))
+                    }
+                }
+                .transition(.slide)
+            }
+            Spacer()
         }
+        .padding(.horizontal)
+        .padding()
     }
 }
 
@@ -25,3 +158,4 @@ struct SignUpView_Previews: PreviewProvider {
         SignUpView()
     }
 }
+
