2ae33ba603784ef2cf6c2d7a6bb562b61b2dd398
Michał Babicz <kiwuzx@gmail.com>
2023-09-13 14:06:08 +0200

    MapView annotation design

--- SwiftMeal/SwiftMeal/Views/MapView.swift
+++ SwiftMeal/SwiftMeal/Views/MapView.swift
@@ -14,17 +14,19 @@ struct MapView: UIViewRepresentable {
 
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
+        mapView.mapType = .standard
         mapView.delegate = context.coordinator
         return mapView
     }
 
     func updateUIView(_ uiView: MKMapView, context: Context) {
         let location = CLLocationCoordinate2D(latitude: currentLatitude, longitude: currentLongitude)
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = location
-
+        uiView.removeOverlays(uiView.overlays)
         uiView.removeAnnotations(uiView.annotations)
-        uiView.addAnnotation(annotation)
+
+        let deliveryAnnotation = MKPointAnnotation()
+        deliveryAnnotation.coordinate = location
+        uiView.addAnnotation(deliveryAnnotation)
 
         let region = MKCoordinateRegion(center: location, latitudinalMeters: 1000, longitudinalMeters: 1000)
         uiView.setRegion(region, animated: true)
@@ -40,9 +42,51 @@ struct MapView: UIViewRepresentable {
         init(_ parent: MapView) {
             self.parent = parent
         }
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            if annotation is MKPointAnnotation {
+                let identifier = "annotation"
+                var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+                
+                if annotationView == nil {
+                    annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                    annotationView?.image = createCircleImage(with: UIColor(named: "ThemeColor"), backgroundColor: .white)
+                    annotationView?.centerOffset = CGPoint(x: 0, y: 0)
+                } else {
+                    annotationView?.annotation = annotation
+                }
+                
+                return annotationView
+            }
+            
+            return nil
+        }
+        
+        func createCircleImage(with fillColor: UIColor?, backgroundColor: UIColor) -> UIImage {
+            let size = CGSize(width: 22, height: 22)
+            let circleRect = CGRect(x: 5, y: 5, width: 12, height: 12)
+            let strokeWidth: CGFloat = 3
+            
+            UIGraphicsBeginImageContextWithOptions(size, false, 0)
+            let context = UIGraphicsGetCurrentContext()!
+            
+            if let fillColor = fillColor {
+                context.setFillColor(fillColor.cgColor)
+            }
+            context.setStrokeColor(backgroundColor.cgColor)
+            context.setLineWidth(strokeWidth)
+            
+            context.fillEllipse(in: circleRect)
+            context.strokeEllipse(in: circleRect)
+            
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            return image!
+        }
+
     }
 }
-
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView(currentLongitude: 45.213132, currentLatitude: 27.23123)
